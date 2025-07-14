1690886b9604f8db7abbb49fb70c3be9a71fc105
NikhilReddySatti <155195347+NikhilReddySatti@users.noreply.github.com>
2025-07-14 17:04:35 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        bg: '#0D1117',
+        accent: '#58A6FF',
+        warn: '#E53E3E',
+        text: '#F0F6FC',
+      },
+    },
+  },
+  plugins: [],
+}
